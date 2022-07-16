2567fd28f0099a320326e90b4d484521066d20cf
Tabata <tabatac.souto@gmail.com>
2022-07-16 12:14:27 -0300

    feat: Configuração arquivo config.js (alteração do nome de config.json para js, e atualização da importação do config no index.js da model)

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DB,
+    host: process.env.MYSQL_HOST,
+    dialect: process.env.DIALECT,
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DB,
+    host: process.env.MYSQL_HOST,
+    dialect: process.env.DIALECT,
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DB,
+    host: process.env.MYSQL_HOST,
+    dialect: process.env.DIALECT,
+  },
+};
