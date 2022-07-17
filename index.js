946be5741935bab44ac0f8b0c4b3d1acc3d53c35
Tabata <tabatac.souto@gmail.com>
2022-07-17 00:16:32 -0300

    feat: endpoind GET/ assets criado.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require('express-async-errors');
 require('dotenv').config();
-
 const express = require('express');
 
-const { PORT } = process.env;
+const router = require('./routes');
 
 const app = express();
+const { PORT } = process.env;
+
+app.use(express.json());
+app.use('/assets', router);
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
