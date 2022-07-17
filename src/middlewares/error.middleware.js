9d267d0a590faf594e19ea06ff40510a105dd841
Tabata <tabatac.souto@gmail.com>
2022-07-16 22:48:07 -0300

    feat: refazendo todo o back-end

--- src/middlewares/error.middleware.js
+++
@@ -1,4 +0,0 @@
-module.exports = (error, _req, res) => {
-  res.status(error.status || 500)
-    .json({ message: error.message || 'Erro inesperado. Por favor, tente mais tarde' });
-};
--- src/services/index.js
+++
@@ -1,7 +0,0 @@
-const { Asset } = require('../database/models');
-
-const getAll = async () => Asset.findAll();
-
-module.exports = {
-  getAll,
-};
